be200a8206bd223907718ecbfb5580f783d134f8
Zachary Graham <zakgraham@gmail.com>
2018-12-23 17:48:38 -0600

    fix array bug

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -10,9 +10,10 @@ public class Main {
 
         MathEquation[] equations = new MathEquation[4];
         equations[0] = create( 100.0d, 50.0d, 'd');
-        equations[0] = create( 25.0d, 90.0d, 'a');
-        equations[0] = create( 225.0d, 17.0d, 's');
-        equations[0] = create( 11.0d, 3.0d, 'm');
+        equations[1] = create( 25.0d, 90.0d, 'a');
+        equations[2] = create( 225.0d, 17.0d, 's');
+        equations[3] = create( 11.0d, 3.0d, 'm');
+
         for(MathEquation equation : equations) {
             equation.execute();
             System.out.print("results = ");
