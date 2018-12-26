c5e321bdd51e1fcd9476d5f6bad5e46c82d27ec4
Zachary Graham <zakgraham@gmail.com>
2018-12-26 16:47:30 -0700

    add constructors to mathequations

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,10 +3,10 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-//        double[] leftVals = { 100.0d, 25.0d, 225.0d, 11.0d };
-//        double[] rightVals = { 50.0d, 92.0d, 17.0d, 3.0d };
-//        char[] opCodes = { 'd', 'a', 's', 'm'};
-//        double[] results = new double[opCodes.length];
+//        MathEquation testEquation = new MathEquation();
+//        testEquation.execute();
+//        System.out.print("test=");
+//        System.out.println(testEquation.getResults());
 
         MathEquation[] equations = new MathEquation[4];
         equations[0] = create( 100.0d, 50.0d, 'd');
