d4a31c15645cf3992a8f9345fce73d56f2ca70a1
Zachary Graham <zgraham@glynlyon.com>
2019-01-02 12:20:34 -0700

    add method overloading

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -23,5 +23,24 @@ public class Main {
         System.out.println();
         System.out.println("Using Overloads");
         System.out.println();
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        int leftInt= 9;
+        int rightInt = 4;
+
+        MathEquation equationOverload = new MathEquation('d');
+
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.print("result=");
+        System.out.println(equationOverload.getResults());
+
+        equationOverload.execute(leftInt, rightInt);
+        System.out.print("result=");
+        System.out.println(equationOverload.getResults());
+
+        equationOverload.execute((double)leftInt, (double)rightInt);
+        System.out.print("result=");
+        System.out.println(equationOverload.getResults());
     }
 }
