f97805d69d46434500073408e70102b3ea367293
Zachary Graham <zakgraham@gmail.com>
2019-01-03 16:16:43 -0700

    create CB constructors

--- src/com/pluralsight/calcengine/CalculateBase.java
+++ src/com/pluralsight/calcengine/CalculateBase.java
@@ -11,5 +11,10 @@ public class CalculateBase {
     public void setRightVal(double rightVal) { this.rightVal = rightVal; }
     public double getResult() { return result; }
     public void setResult(double result) {this.result = result;}
-    
+
+    public CalculateBase() {};
+    public CalculateBase(double leftVal, double rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
 }
