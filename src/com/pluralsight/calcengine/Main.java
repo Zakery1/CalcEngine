d32150eecdeef292494c2e05fe767e0f6b21d4f5
Zachary Graham <zakgraham@gmail.com>
2019-01-08 16:42:17 -0700

    add operation strings to main

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,10 +3,15 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-//        MathEquation testEquation = new MathEquation();
-//        testEquation.execute();
-//        System.out.print("test=");
-//        System.out.println(testEquation.getResults());
+
+
+        String[] statements = {
+                "divide 100.0 50.0",
+                "add 25.0 92.0",
+                "subtract 225.0 17.0",
+                "multiply 11.0 3.0"
+        };
+
 
         MathEquation[] equations = new MathEquation[4];
         equations[0] = new MathEquation('d', 100.0d, 50.0d);
@@ -42,5 +47,22 @@ public class Main {
         equationOverload.execute((double)leftInt, (double)rightInt);
         System.out.print("result=");
         System.out.println(equationOverload.getResults());
+
+        System.out.println("");
+        System.out.println("Using Inheritance");
+        System.out.println("");
+
+        CalculateBase[] calculators = {
+                new Divider(100.0d, 50.0d),
+                new Adder(25.0d, 92.0d),
+                new Subtracter(225.0d, 17.0d),
+                new Multiplier(11.0d,3.0d)
+        };
+
+        for(CalculateBase calculator: calculators) {
+            calculator.calculate();
+            System.out.print("result=");
+            System.out.println(calculator.getResult());
+        }
     }
 }
